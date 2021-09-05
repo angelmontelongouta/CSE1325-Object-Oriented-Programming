d2ca34a6df13632cc7237a882fbbf9d1c10c674c
angelmontelongouta <angel.montelongo@mavs.uta.edu>
2021-09-04 23:48:14 -0500

    P02 - Starting Bonus

---
+++ P02/bonus/OnARoll.java
@@ -0,0 +1,37 @@
+import java.util.Scanner; 
+import java.util.Arrays;
+
+public class OnARoll 
+{
+    public static void main(String[] args) 
+	{
+		
+		int rolls = Integer.parseInt(args[0]);
+		int numFaces = Integer.parseInt(args[1]);
+		
+		int[] rollsArray;
+		rollsArray = new int[rolls];
+		int sum = 0; 
+		
+		for(int j = 0; j < rolls; j++)
+		{
+			double numRolled = (Math.random()*numFaces);
+			int numRolledInt = (int)numRolled+1;
+			rollsArray[j] = numRolledInt;
+			
+			sum = sum + rollsArray[j];
+		}
+		
+		Arrays.sort(rollsArray);
+		
+		for(int i = 0; i < rolls; i++)
+		{
+			System.out.print(rollsArray[i]+" ");
+		}
+		System.out.println();
+		
+		System.out.println("Sum = " +sum);
+		double average = (double)sum/rolls;
+		System.out.println("Average = " +average);
+    }
+}
