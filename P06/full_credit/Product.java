1f4a3751c7c1f6c5a67e7cc24aafe9574d9bd010
angelmontelongouta <angel.montelongo@mavs.uta.edu>
2021-10-12 02:34:11 -0500

    full_credit

---
+++ P06/full_credit/Product.java
@@ -0,0 +1,25 @@
+public abstract class Product 
+{
+	public Product(String name, double price, double cost)
+	{
+		this.name = name;
+		this.price = price;
+		this.cost = cost;
+	}
+	
+	public String name()
+	{
+		return name;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String result = name + " $" + price;
+		return result;
+	}
+	
+	protected String name;
+	protected double price;
+	protected double cost;
+}
